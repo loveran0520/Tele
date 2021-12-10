4cfc77c606726ad33d32883851b154e282647032
loveran0520 <62301420+loveran0520@users.noreply.github.com>
2021-12-10 19:52:21 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,27 +6,56 @@ from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser
 from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
 from telethon.tl.functions.channels import InviteToChannelRequest
-import sys
+import os, sys
+import configparser
 import csv
-import traceback
 import time
 from datetime import datetime
-api_id = 
-api_hash = ''
-phone = ''
-client = TelegramClient(phone, api_id, api_hash)
+import traceback
+
+re="\033[1;31m"
+gr="\033[1;32m"
+cy="\033[1;36m"
+
+def banner():
+    print(f"""
+{re}╔╦╗{cy}┌─┐┬  ┌─┐{re}╔═╗  ╔═╗{cy}┌─┐┬─┐┌─┐┌─┐┌─┐┬─┐
+{re} ║ {cy}├┤ │  ├┤ {re}║ ╦  ╚═╗{cy}│  ├┬┘├─┤├─┘├┤ ├┬┘
+{re} ╩ {cy}└─┘┴─┘└─┘{re}╚═╝  ╚═╝{cy}└─┘┴└─┴ ┴┴  └─┘┴└─
+
+              Version : 1.01
+ {re}Subscribe Termux Professor on Youtube.
+   {cy}www.youtube.com/c/TermuxProfessorYT
+        """)
+
+cpass = configparser.RawConfigParser()
+cpass.read('config.data')
+
+try:
+    api_id = cpass['cred']['id']
+    api_hash = cpass['cred']['hash']
+    phone = cpass['cred']['phone']
+    client = TelegramClient(phone, api_id, api_hash)
+except KeyError:
+    os.system('clear')
+    banner()
+    print(re+"[!] run python3 setup.py first !!\n")
+    sys.exit(1)
 
 client.connect()
 if not client.is_user_authorized():
     client.send_code_request(phone)
-    client.sign_in(phone, input('Enter the code recieved to your Telegram messenger: '))
-
-
+    os.system('clear')
+    banner()
+    client.sign_in(phone, input(gr+'[+] Enter the code: '+re))
+ 
+os.system('clear')
+banner()
 chats = []
 last_date = None
 chunk_size = 200
 groups=[]
-
+ 
 result = client(GetDialogsRequest(
              offset_date=last_date,
              offset_id=0,
@@ -35,32 +64,35 @@ result = client(GetDialogsRequest(
              hash = 0
          ))
 chats.extend(result.chats)
-
+ 
 for chat in chats:
     try:
         if chat.megagroup== True:
             groups.append(chat)
     except:
         continue
-
-print('Choose a group to scrape members from:')
+ 
+print(gr+'[+] Choose a group to scrape members :'+re)
 i=0
 for g in groups:
-    print(str(i) + '- ' + g.title)
+    print(gr+'['+cy+str(i)+']' + ' - ' + g.title)
     i+=1
-
-g_index = input("Enter a Number: ")
+ 
+print('')
+g_index = input(gr+"[+] Enter a Number : "+re)
 target_group=groups[int(g_index)]
-
-print('Fetching Members...')
+ 
+print(gr+'[+] Fetching Members...')
+time.sleep(1)
 all_participants = []
 all_participants = client.get_participants(target_group, aggressive=True)
-
-print('Saving In file...')
+ 
+print(gr+'[+] Saving In file...')
+time.sleep(1)
 with open("members.csv","w",encoding='UTF-8') as f:
     writer = csv.writer(f,delimiter=",",lineterminator="\n")
-    writer.writerow(['username','user id', 'access hash','name','group', 'group id','last seen'])
-    for user in all_participants:
+    writer.writerow(['username','user id', 'access hash','name','group', 'group id'])
+   for user in all_participants:
         accept=True
         try:
             lastDate=user.status.was_online
@@ -87,6 +119,3 @@ with open("members.csv","w",encoding='UTF-8') as f:
             writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id,user.status]) 
 
 print('Members scraped successfully.')
-Share
-Improve this answer
-Follow
