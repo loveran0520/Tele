6a8b29a3220a3abeb4ce172c0047fe76974ebe11
loveran0520 <62301420+loveran0520@users.noreply.github.com>
2021-12-10 19:39:00 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,103 +1,114 @@
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
-from telethon.tl.types import (
-    InputPeerEmpty,
-    UserStatusLastWeek,
-    UserStatusOnline,
-    UserStatusRecently,
-)
+from telethon.tl.types import InputPeerEmpty
+import os, sys
+import configparser
 import csv
-from datetime import date, datetime
+import time
 
-api_id =
-api_hash = ''
-phone = '+'
-client = TelegramClient(phone, api_id, api_hash)
+re="\033[1;31m"
+gr="\033[1;32m"
+cy="\033[1;36m"
+
+def banner():
+    print(f"""
+{re}╔╦╗{cy}┌─┐┬  ┌─┐{re}╔═╗  ╔═╗{cy}┌─┐┬─┐┌─┐┌─┐┌─┐┬─┐
+{re} ║ {cy}├┤ │  ├┤ {re}║ ╦  ╚═╗{cy}│  ├┬┘├─┤├─┘├┤ ├┬┘
+{re} ╩ {cy}└─┘┴─┘└─┘{re}╚═╝  ╚═╝{cy}└─┘┴└─┴ ┴┴  └─┘┴└─
+
+              Version : 1.01
+ {re}Subscribe Termux Professor on Youtube.
+   {cy}www.youtube.com/c/TermuxProfessorYT
+        """)
+
+cpass = configparser.RawConfigParser()
+cpass.read('config.data')
+
+try:
+    api_id = cpass['cred']['id']
+    api_hash = cpass['cred']['hash']
+    phone = cpass['cred']['phone']
+    client = TelegramClient(phone, api_id, api_hash)
+except KeyError:
+    os.system('clear')
+    banner()
+    print(re+"[!] run python3 setup.py first !!\n")
+    sys.exit(1)
+
 client.connect()
 if not client.is_user_authorized():
     client.send_code_request(phone)
-    client.sign_in(phone, input('Enter the code: '))
+    os.system('clear')
+    banner()
+    client.sign_in(phone, input(gr+'[+] Enter the code: '+re))
+ 
+os.system('clear')
+banner()
 chats = []
 last_date = None
 chunk_size = 200
-groups = []
-
-result = client(
-    GetDialogsRequest(
-        offset_date=last_date,
-        offset_id=0,
-        offset_peer=InputPeerEmpty(),
-        limit=chunk_size,
-        hash=0,
-    )
-)
+groups=[]
+ 
+result = client(GetDialogsRequest(
+             offset_date=last_date,
+             offset_id=0,
+             offset_peer=InputPeerEmpty(),
+             limit=chunk_size,
+             hash = 0
+         ))
 chats.extend(result.chats)
-
+ 
 for chat in chats:
     try:
-        if chat.megagroup == True:
+        if chat.megagroup== True:
             groups.append(chat)
     except:
         continue
-
-print('Choose a group to scrape members from:')
-i = 0
+ 
+print(gr+'[+] Choose a group to scrape members :'+re)
+i=0
 for g in groups:
-    print(str(i) + '- ' + g.title)
-    i += 1
-
-g_index = input("Enter a Number: ")
-target_group = groups[int(g_index)]
-
-print('Fetching Members...')
+    print(gr+'['+cy+str(i)+']' + ' - ' + g.title)
+    i+=1
+ 
+print('')
+g_index = input(gr+"[+] Enter a Number : "+re)
+target_group=groups[int(g_index)]
+ 
+print(gr+'[+] Fetching Members...')
+time.sleep(1)
 all_participants = []
 all_participants = client.get_participants(target_group, aggressive=True)
-print(all_participants)
-
-print('Saving In file...')
-with open("members.csv", "w", encoding='UTF-8') as f:
-    writer = csv.writer(f, delimiter=",", lineterminator="\n")
-    writer.writerow(
-        ['username', 'user id', 'access hash', 'name', 'group', 'group id', 'last seen']
-    )
+ 
+print(gr+'[+] Saving In file...')
+time.sleep(1)
+with open("members.csv","w",encoding='UTF-8') as f:
+    writer = csv.writer(f,delimiter=",",lineterminator="\n")
+    writer.writerow(['username','user id', 'access hash','name','group', 'group id'])
     for user in all_participants:
-        accept = True
+        accept=True
         try:
-            lastDate = user.status.was_online
-            num_months = (datetime.now().year - lastDate.year) * 12 + (
-                datetime.now().month - lastDate.month
-            )
-            if num_months > 0.5:
-                accept = False
+            lastDate=user.status.was_online
+            num_months = (datetime.now().year - lastDate.year) * 12 + (datetime.now().month - lastDate.month)
+            if(num_months>1):
+                accept=False
         except:
             continue
 
-        if not user.status == UserStatusRecently():
-            accept = False
-        if accept:
+        if (accept) :
             if user.username:
-                username = user.username
+                username= user.username
             else:
-                username = ""
+                username= ""
             if user.first_name:
-                first_name = user.first_name
+                first_name= user.first_name
             else:
-                first_name = ""
+                first_name= ""
             if user.last_name:
-                last_name = user.last_name
+                last_name= user.last_name
             else:
-                last_name = ""
-            name = (first_name + ' ' + last_name).strip()
-            writer.writerow(
-                [
-                    username,
-                    user.id,
-                    user.access_hash,
-                    name,
-                    target_group.title,
-                    target_group.id,
-                    user.status,
-                ]
-            )
+                last_name= ""
+            name= (first_name + ' ' + last_name).strip()
+            writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id,user.status]) 
 
-print('Members scraped successfully.')
+print(gr+'[+] Members scraped successfully. Subscribe Termux Professor Youtube Channel For Add Members')
